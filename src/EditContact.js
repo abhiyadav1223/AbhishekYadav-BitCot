9434181b89d0bc3ce04a5e60a370480bc1685763
abhiyadav1223 <yadavabhi2324@gmail.com>
2023-06-28 22:22:54 +0530

    Test Bitcote card operation

--- src/EditContact.js
+++ src/EditContact.js
@@ -61,4 +61,4 @@ export default function EditContact(){
       </form>
     </div>
   </>
-  }
+  } 
--- src/addContact.js
+++ src/addContact.js
@@ -55,4 +55,4 @@ export default function AddContact() {
       </form>
     </div>
   </>
-}
+} 
